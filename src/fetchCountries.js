17ee8923d30fa25b1b6084dc66f378025bb5f4a1
IvanSkliar <skliariv91@gmail.com>
2022-05-23 15:43:28 +0300

    dz10/3

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+const BASE_URL = `https://restcountries.com`;
+
+
+ export function fetchCountries(name) {
+      return fetch(`${BASE_URL}/v3.1/name/${name}?fields=flags,name,capital,population,languages`)
+     .then((response) => {
+         if (response.status === 200) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+     })
+      .catch(error => console.err(error));
+}; 
